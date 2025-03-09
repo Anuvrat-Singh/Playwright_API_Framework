ff0c0043d10e8eea3c6845cdde432c10fd3fd8d1
Anuvrat-Singh <anuvratsingh91@gmail.com>
2025-03-09 14:39:42 +0530

    added assertions on static data

--- tests/API_Testing/post_static_data.spec.ts
+++ tests/API_Testing/post_static_data.spec.ts
@@ -2,16 +2,37 @@
 import { test, expect } from "@playwright/test";
 import postRequestData from "../../test-data/api_requests/post_example.json";
 test.use({
-    baseURL: process.env.BASE_API_URL,
-  });
+  baseURL: process.env.BASE_API_URL,
+});
 /**
  * Author: Anuvrat Singh
  */
 test("Post request using static data", async ({ request }) => {
+  //create post api request and save it in response
   const response = await request.post(`/booking`, { data: postRequestData });
-  const jsonResponse = JSON.stringify(response, null, 2);
-  console.log("Response object in json format: " + jsonResponse);
-
+  //convert response into json format
+  const jsonResponse = await response.json();
+  //log the response for debugging
+  console.log(
+    "Response object in json format: " + JSON.stringify(jsonResponse, null, 2)
+  );
+  //perform validations
+  //use api response object to perform api response level validations
   expect(response.status()).toBe(200);
   expect(response.statusText()).toBe("OK");
+  expect(response.headers()["content-type"]).toContain("application/json");
+
+  //validate property/key in response
+  //use jsonResponse object to perform json response level validation of api call
+  expect(jsonResponse.booking).toHaveProperty("firstname");
+  expect(jsonResponse.booking).toHaveProperty("lastname");
+  expect(jsonResponse.booking.bookingdates).toHaveProperty("checkin");
+  expect(jsonResponse.booking.bookingdates).toHaveProperty("checkout");
+
+  //validate api response body -> actual values in the response body
+  expect(jsonResponse).toHaveProperty("bookingid");
+  expect(jsonResponse.booking.firstname).toBe("Anuvrat");
+  expect(jsonResponse.booking.lastname).toBe("Singh");
+  expect(jsonResponse.booking.bookingdates.checkin).toBe("2025-01-01");
+  expect(jsonResponse.booking.bookingdates.checkout).toBe("2025-01-02");
 });
